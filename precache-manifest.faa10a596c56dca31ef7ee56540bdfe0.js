fec7e54a13f080585012618b5d544953554e34a7
Hrithik <hc388@njit.edu>
2020-07-29 02:24:28 -0400

    Updates

---
+++ precache-manifest.faa10a596c56dca31ef7ee56540bdfe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34add6e7909901bc733bda006903ed9c",
+    "url": "/react-trial/index.html"
+  },
+  {
+    "revision": "ba1c98303d3ed1c6bcd5",
+    "url": "/react-trial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe0e65749378c627d557",
+    "url": "/react-trial/static/js/2.9de217ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-trial/static/js/2.9de217ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1c98303d3ed1c6bcd5",
+    "url": "/react-trial/static/js/main.0e1228af.chunk.js"
+  },
+  {
+    "revision": "fdceea1d75a2a4476d7c",
+    "url": "/react-trial/static/js/runtime-main.104bc648.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
